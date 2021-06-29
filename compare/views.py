62332b1aa0fc0b776594d0974ca324713a07659f
jhwarnecke <jhwarnecke@posteo.de>
2021-06-29 22:25:12 +0200

    a

--- compare/views.py
+++ compare/views.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from os import name
+from os import error, name
 from django.shortcuts import render
 import numpy as np
 import pandas as pd
@@ -22,23 +22,102 @@ def index(request):
 
 
 def calculation(request):
+    global monday
+    monday = False
+    global tuesday
+    tuesday = False
+    global wednesday
+    wednesday = False
+    global thursday
+    thursday = False
+    global friday
+    friday = False
+    global saturday
+    saturday = False
+    global sunday
+    sunday = False
+    global monday2
+    monday2 = False
+    global tuesday2
+    tuesday2 = False
+    global wednesday2
+    wednesday2 = False
+    global thursday2
+    thursday2 = False
+    global friday2
+    friday2 = False
+    global saturday2
+    saturday2 = False
+    global sunday2
+    sunday2 = False
 
     #time_of_use = sum(time_dict.values())
     time_per_use = int(request.POST['usetime'])
     uses_per_day = int(request.POST['usesday'])
-    days = int(request.POST['days'])
-    uses = days * uses_per_day
+
+    day = request.POST.getlist("day")
+    day2 = request.POST.getlist("day2")
+
+    if len(day) == 0:
+        return(render(request, "error.html"))
+
+
+    for z in range(0, len(day)):
+        if day[z] == "monday":
+            monday = True
+        if day[z] == "tuesday":
+            tuesday = True
+        if day[z] == "wednesday":
+            wednesday = True
+        if day[z] == "thursday":
+            thursday = True
+        if day[z] == "friday":
+            friday = True
+        if day[z] == "saturday":
+            saturday = True
+        if day[z] == "sunday":
+            sunday = True
+
+    if day2 is not None:
+        for z in range(0, len(day2)):
+            if day2[z] == "monday":
+                monday2 = True
+            if day2[z] == "tuesday":
+                tuesday2 = True
+            if day2[z] == "wednesday":
+                wednesday2 = True
+            if day2[z] == "thursday":
+                thursday2 = True
+            if day2[z] == "friday":
+                friday2 = True
+            if day2[z] == "saturday":
+                saturday2 = True
+            if day2[z] == "sunday":
+                sunday2 = True
+
+    
+    
+    numdays = len(day)
+    numdays2 = len(day2)
+
+
+    uses = numdays * uses_per_day
     total_time = time_per_use * uses
 
     time_per_use2 = int(request.POST['usetime2'])
     uses_per_day2 = int(request.POST['usesday2'])
-    days2 = int(request.POST['days2'])
-    uses2 = days2 * uses_per_day2
+    uses2 = numdays2 * uses_per_day2
     total_time2 = time_per_use2 * uses2
-    samedays = int(request.POST['samedays'])
+
+    samedays = 0
+    for z in range(0, len(day)):
+        for y in range(0, len(day2)):
+            if day[z] == day2[y]:
+                samedays += 1
 
 
-    days_sum = days + days2 - samedays
+
+    days_sum = numdays + numdays2 - samedays
     uses_sum = uses + uses2
     total_time_sum = total_time + total_time2
     
@@ -49,7 +128,7 @@ def calculation(request):
 
     # iterations for all the models, save costs and name in df
     for i in range(0, x[1]-1):
-        total_costs = round(tester(i, days_sum, uses_sum, total_time_sum), 2)
+        total_costs = round(normal(i, days_sum, uses_sum, total_time_sum), 2)
         mytempname = ((df[0][i + 2])+" "+(df[1][i + 2]))
         mytempdf = pd.DataFrame({'Name': [mytempname], 'Kosten': [total_costs]})
         mydf = mydf.append(mytempdf, ignore_index=True)
@@ -78,20 +157,41 @@ def calculation(request):
 
 # definition of the cost function
 # missing variables for (Beschränkung Zeit, Preis nach 45)
-def tester(j, days, uses, total_time):
+def normal(j, days, uses, total_time):
     grund = df[2][j+1] * uses
     preispmin = df[3][j+1] * total_time
     preisppak = df[4][j+1]
     preispmon = df[5][j+1]
     preisptag = df[6][j+1] * days
-    konti = kontingent(j+1, total_time)
-    costs = grund + preispmin + preisptag + preisppak + preispmon + konti
+    mylist = kontingent(j+1, total_time)
+    konti = mylist[0]
+    anteil_rest_konsten_konti = mylist[1]
+    konti_plus = konti - anteil_rest_konsten_konti
+    costs = grund + preispmin + preisptag + preisppak + preispmon + konti_plus
     return costs
 
-# definition für tester von kontingent
+# def für normal von kontingent
+# Der Montag wird als Buchungszeitpunkt für den Konti-Tarif angesehen (sollte aber keinen großen Einfluss haben)
 def kontingent(j, total_time):
     if df[7][j] != 0 and df[7][j] < total_time:
-        konti = (total_time - df[7][j]) * df[8][j]
+        konti = 0
+        loop = 1
+        new_total_time = total_time
+        while loop == 1:
+            konti = konti + df[4][j]
+            new_total_time = new_total_time - df[7][j]
+            if df[7][j] < new_total_time:
+                loop = 1
+            else:
+                loop = 0
+                rest =  df[7][j] - new_total_time
+                anteil_rest = rest / total_time
+                anteil_rest_kosten = anteil_rest * df[4][j]
+                mylist = [konti, anteil_rest_kosten]
+
     else:
         konti = 0
-    return konti
+        anteil_rest_kosten = 0
+        mylist = [konti, anteil_rest_kosten]
+
+    return mylist
