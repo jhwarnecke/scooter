1fd04fe9280cbbbc1e62852bbbfd9ff171c7aca4
jhwarnecke <jhwarnecke@posteo.de>
2021-07-20 16:25:29 +0200

    a

--- compare/views.py
+++ compare/views.py
@@ -4,10 +4,7 @@ from django.shortcuts import render
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import io
 import base64
-import cgitb
-from pandas.io.formats import style
 from matplotlib import pylab
 from io import BytesIO
 from datetime import datetime
@@ -15,16 +12,24 @@ from openpyxl import load_workbook
 
 
 
-# reading excel sheet, index_col = none so we can get indices instead of names as headers
-# that's why we will always start with row '1' and not '0'
-# for columns we will start with '2' because there the values (variables) start
-df = pd.read_excel(r'scooter.xlsx', index_col=None, header=None)
-# get shape of excel sheet (to know number of iterations)
-x = df.shape
+# # reading excel sheet, index_col = none so we can get indices instead of names as headers
+# # that's why we will always start with row '1' and not '0'
+# # for columns we will start with '2' because there the values (variables) start
+# df = pd.read_excel(r'scooter.xlsx', index_col=None, header=None)
+# # get shape of excel sheet (to know number of iterations)
+# x = df.shape
 
 
 
 def index(request):
+    # reading excel sheet, index_col = none so we can get indices instead of names as headers
+    # that's why we will always start with row '1' and not '0'
+    # for columns we will start with '2' because there the values (variables) start
+    global df
+    df = pd.read_excel(r'scooter.xlsx', index_col=None, header=None)
+    # get shape of excel sheet (to know number of iterations)
+    global x
+    x = df.shape
     dx = df.to_html(header=False, index=False)
     return render(request, "compare/input.html", {"metable":dx})
 
--- edittable/urls.py
+++ edittable/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='editindex'),
     path('editfunc', views.editfunc, name='editfunc') 
 
 ]
